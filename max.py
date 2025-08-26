7f989a26a15102df19f453b7fe0a3e849405de29
Moataz0000 <moatazfawzy150@gmail.com>
2025-08-27 02:59:48 +0300

    recursive max

---
+++ max.py
@@ -0,0 +1,20 @@
+from selectors import SelectSelector
+
+from setuptools.command.build_ext import if_dl
+
+N = int(input().strip())
+
+numbers = list(map(int, input().split()))
+
+
+def getMax(nums: list) -> int:
+    if len(nums) == 0:
+        return 0
+    elif len(nums) == 1:
+        return nums[0]
+
+    max_of_rest = getMax(nums[1:])
+    return nums[0] if nums[0] > max_of_rest else max_of_rest
+
+
+print(getMax(numbers))
