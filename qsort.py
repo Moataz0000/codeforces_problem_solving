0b253f0d7fb232b5c82e7654d9f8dedc35001f9c
Moataz0000 <moatazfawzy150@gmail.com>
2025-09-11 00:19:22 +0300

    solved

--- qsort.py
+++ qsort.py
@@ -1,27 +1,14 @@
-import time
-import random
-
-
 def qsort(arr: list) -> list:
 
     if len(arr) < 2: # Base Case
         return arr
     
-    pivot = arr[0]
+    pivot = arr[0] # Recursive Case
     less = [i for i in arr[1:] if i <= pivot]  
     greater = [i for i in arr[1:] if i > pivot]  
     
     return qsort(less) + [pivot] + qsort(greater)  
 
     
+print(qsort([4, 2, 7, 1, 3, 5,10]))
 
-unsorted_list = [random.randint(1, 1000000000) for _ in range(1000)]
-
-start = time.perf_counter()
-q = qsort(arr=unsorted_list)
-elapsed = time.perf_counter() - start
-
-
-print(f"Elapsed time for qsort on 1000 ints: {elapsed*1000:.3f} ms")
-print("Matches built-in sorted():", q == sorted(unsorted_list))
-print(q)
