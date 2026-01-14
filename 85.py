8f104105ae2f4bf756e70345e9f763e38d62a81f
Moataz0000 <moatazfawzy150@gmail.com>
2026-01-15 00:02:58 +0200

    feat: solved

---
+++ 85.py
@@ -0,0 +1,87 @@
+
+
+def get_length(arr: list) -> int:
+    counter = 0
+    for x in arr:
+        counter += 1
+    return counter
+
+def get_max(arr: list) -> int:
+    random = arr[0]
+    for i in range(get_length(arr)):
+        if arr[i] > random:
+            random = arr[i]
+        else:
+            continue
+    return random
+
+def get_min(arr: list) -> int:
+    random = arr[0]
+    for j in range(get_length(arr)):
+        if arr[j] < random:
+            random = arr[j]
+        else:
+            continue
+    return random
+
+def get_prime(arr: list) -> int:
+    def is_prime(num: int) -> bool:
+        if num < 2:
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+    counter: int = 0
+    for n in arr:
+        if is_prime(n):
+            counter += 1
+    return counter
+  
+def get_palindrome(arr: list) -> int:
+    def is_palindrome(num: int) -> bool:
+        num = str(num)
+        if num == num[::-1]:
+            return True
+        return False
+    
+    counter: int = 0
+    for n in arr:
+        if is_palindrome(n):
+            counter += 1
+    return counter
+
+    
+def get_max_divisors(arr: list) -> int:
+    def count_divisore(num: int) -> int:
+        counter: int = 0
+        for n in range(1, num):
+            if num % n == 0:
+                counter += 1
+        return counter
+    
+    max_divs: int = -1
+    best_num: int = -1
+
+    for i in arr:
+        current_divs = count_divisore(i)
+
+        if current_divs > max_divs:
+            max_divs = current_divs
+            best_num = i
+        elif current_divs == max_divs:
+            if i > best_num:
+                best_num = i
+    return best_num
+                
+    
+
+N = int(input())
+A = list(map(int, input().split()))
+print(f"The maximum number : {get_max(A)}")
+print(f"The minimum number : {get_min(A)}")
+print(f"The number of prime numbers : {get_prime(A)}")
+print(f"The number of palindrome numbers : {get_palindrome(A)}")
+print(f"The number that has the maximum number of divisors : {get_max_divisors(A)}")
+
