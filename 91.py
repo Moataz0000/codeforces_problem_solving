f32cbabb22d8da2fceb2de98f418dc4d4448cf6b
Moataz0000 <moatazfawzy150@gmail.com>
2026-01-20 22:30:30 +0200

    feat: solved

---
+++ 91.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        k = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[k] = nums[i]
+                k += 1
+        
+        return k        
+
+
+
+nums = [1,1,2]
+
+obj = Solution()
+print(obj.removeDuplicates(nums))
+        
