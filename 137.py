3e9bd7b9b17c6a2282656665dc920dee7da552ae
Moataz0000 <moatazfawzy150@gmail.com>
2026-04-09 01:07:25 +0200

    feat: sovled

---
+++ 137.py
@@ -0,0 +1,30 @@
+
+
+
+
+# def find_pair_with_sum(arr: list, target: int) -> bool:
+#     for i in range(len(arr)):
+#         for j in range(i+1, len(arr)):
+#             if arr[i] + arr[j] == target:
+#                 return True
+#     return False
+
+
+# print(find_pair_with_sum(arr=[1,2,3], target=3))
+
+
+def find_pair_with_sum(arr: list, target: int) -> bool:
+
+    left, right = 0, len(arr) - 1 # first and last indexs
+
+    while left < right:
+        s = arr[left] + arr[right]
+        if s == target:
+            return True
+        elif s < target: # less then
+            left += 1 # move one step
+        else:
+            right -= 1 # miunse one step
+    return False
+
+print(find_pair_with_sum(arr=[1,2,3], target=3))
